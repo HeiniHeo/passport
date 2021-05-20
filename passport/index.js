70b1bc60fe199b7a6166db25ff85a7866c42c2ec
heini <melme900@gmail.com>
2021-05-20 14:17:53 +0900

    passport

---
+++ passport/index.js
@@ -0,0 +1,4 @@
+/* HTML에서 POST 로 아이디와 패스워드 받았을때 처리하는 로직 || 전략 */ 
+
+const LocalStrategy= require('passport-local').Strategy
+
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const app = express();
+
+app.use((req,res,next)=>{
+    req.eez = 'At'
+    req.heini = 'heini';
+    next();
+})
+
+app.use(session({
+    secret : 'the', 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/',(req,res)=>{
+    console.log(req.session)
+    res.send(`hello world ${req.heini} & ${req.eez}`)
+})
+
+app.get('/user',(req,res)=>{
+    res.send(`hello world ${req.heini} & ${req.eez}`)
+})
+
+app.get('/board',(req,res)=>{
+    res.send(`hello world ${req.heini} & ${req.eez}`)
+})
+
+app.listen(3000,()=>{
+    console.log('open')
+})
